6c31be905572e54eac43b9a3bdde3431eb9cafb0
emalfano <emalfano1@gmail.com>
2015-04-19 11:43:50 -0700

    emy

---
+++ Demo1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Demo1
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDemo = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDemo
+            // 
+            this.btnDemo.Location = new System.Drawing.Point(239, 152);
+            this.btnDemo.Name = "btnDemo";
+            this.btnDemo.Size = new System.Drawing.Size(272, 197);
+            this.btnDemo.TabIndex = 0;
+            this.btnDemo.Text = "Show Output";
+            this.btnDemo.UseVisualStyleBackColor = true;
+            this.btnDemo.Click += new System.EventHandler(this.btnDemo_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(176, 459);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(415, 31);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(176, 536);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(150, 25);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Demo1 results";
+            // 
+            // frmDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 703);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnDemo);
+            this.Name = "frmDemo";
+            this.Text = " Demo1 Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDemo;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Demo1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo1
+{
+    public partial class frmDemo : Form
+    {
+        public frmDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDemo_Click(object sender, EventArgs e)
+        {
+            int radius = 4;
+            const double PI = 3.14;
+            double volume;
+            // Do calculation for volume of a sphere
+            volume = 4.0 / 3.0 * PI * radius * radius * radius;
+            txtOutput.Text = "Radius = " + radius + ", PI = " + PI;
+            txtOutput.Text = txtOutput.Text + " volume = "
+                + volume.ToString("##.00");
+        }
+
+
+
+
+    }
+}
